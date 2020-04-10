964601744fb3492b96e82f2c46ce23162325fe6c
Nwani Victory <vickywane@gmail.com>
2020-04-10 14:12:45 +0100

    setup basic stuff

---
+++ pages/home/home.vue
@@ -0,0 +1,77 @@
+<template>
+  <div class="body">
+    <BCard class="card">
+      <div>
+        <br />
+        <label>Current Population</label>
+        <input placeholder="Current Population" type="number" />
+        <br />
+        <br />
+        <label>Expected Time to Elapse</label>
+        <input placeholder="Time to Elapse" type="number" />
+        <br />
+        <br />
+        <label>Total Reported Cases</label>
+        <input placeholder="Reported Cases " type="number" />
+        <br />
+        <br />
+        <label>Total Reported Cases</label>
+        <input placeholder="Total Hosiptal Beds" type="number" />
+      </div>
+      <br />
+      <label for="period">Period Type :</label>
+      <select id="period">
+        <option value="days">Days</option>
+        <option value="weeks">Weeks</option>
+        <option value="months">Months</option>
+      </select>
+      <br />
+      <br />
+      <button>Estimate Data</button>
+    </BCard>
+  </div>
+</template>
+
+<script>
+import { BCard } from "bootstrap-vue";
+
+export default {
+  name: "Home"
+};
+</script>
+
+<style scoped lang="postcss" >
+.body {
+  padding: 1rem;
+  justify-content: center;
+  display: flex;
+  place-items: center;
+}
+
+.card {
+  box-shadow: 0px 3px 5px grey;
+  width: 40rem;
+
+  & div {
+    text-align: center;
+    & label {
+      text-align: left;
+      font-size: 1.1rem;
+    }
+    & input {
+      width: 32rem;
+      border: 1px solid #000;
+      height: 5.5vh;
+      border-radius: 4px;
+      padding: 0.7rem 1.5rem;
+    }
+  }
+  & button {
+    background: transparent;
+    color: #000;
+    padding: 0.7rem 5rem;
+    border: 1px solid #000;
+    border-radius: 5px;
+  }
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <Header />
+    <Home />
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import Header from "../components/head.vue";
+import Home from "./home/home.vue";
+
+export default {
+  name: "Index",
+  components: {
+    Header,
+    Home
+  }
+};
+</script>
+
+<style>
+/* .container {
+  margin: 0 auto;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+} */
+</style>
